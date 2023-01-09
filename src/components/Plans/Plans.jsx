4ae4319d0949d919bc55c0cb19c81a699bad0385
Gent Mulaj <mulajgent2001@gmail.com>
2023-01-10 00:35:52 +0100

    .

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {plansData} from '../../data/plansData'
+import whiteTick from '../../assets/whiteTick.png'
+import './Plans.css'
+
+const Plans = () => {
+  return (
+    <div className="plans-container" id='plans'>
+        <div className="blur plans-blur-1"></div>
+        <div className="blur plans-blur-2"></div>
+        <div className="programs-header">
+            <span className='stroke-text'>Ready to start</span>
+            <span>your journey</span>
+            <span className='stroke-text'>now withus</span>
+        </div>
+
+        {/* Plans card */}
+        <div className="plans">
+            {plansData.map((plan,i) => (
+                <div className="plan" key={i}>
+                    {plan.icon}
+                    <span>{plan.name}</span>
+                    <span>$ {plan.price}</span>
+
+                    <div className="features">
+                        {plan.features.map((feature, i) => (
+                            <div className="feature">
+                                <img src={whiteTick} alt="" />
+                                <span key={i}>{feature}</span>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div>
+                        <span>See more benefits -> </span>
+                    </div>
+                    <button className='btn'>Join now</button>
+                </div>
+            ))}
+        </div>
+
+    </div>
+  )
+}
+
+export default Plans
